7ba4c5f714a071ecf445fc4520920fd541ce9bb7
Nikita A. <95999072+druce1@users.noreply.github.com>
2024-12-15 13:24:45 +0300

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -45,14 +45,14 @@ class Library:
 
 
 if __name__ == '__main__':
-    empty_library = Library()  # инициализируем пустую библиотеку
-    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+    empty_library = Library()
+    print(empty_library.get_next_book_id())
 
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
         for book_dict in BOOKS_DATABASE
     ]
-    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
-    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+    library_with_books = Library(books=list_books)
+    print(library_with_books.get_next_book_id())
 
-    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+    print(library_with_books.get_index_by_book_id(1))
